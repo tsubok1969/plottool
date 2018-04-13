81f6356d92bc0b0a20d94790b110ec4a78170868
Ken Tsubouchi <tsubo@TsuboMB.local>
2018-04-13 16:30:36 +0900

    tmp_fig modified

--- tmp_fig.py
+++ tmp_fig.py
@@ -1,29 +1,29 @@
+from datahandle import *
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ptick
+
 def makefig(jobid, mpi=True, pui=True, endian=True, npui=False, ymin=0, ymax=800, t1=200, t2=600, t3=1400, vmin=0, vmax=1.):
-    from datahandle import *
-    import matplotlib.pyplot as plt
-    import matplotlib.ticker as ptick
     job = job2d(jobid, mpi=mpi, pui=pui, endian=endian)
-    df1 = fld2D(job, t1, pui=pui)
-    df2 = fld2D(job, t2, pui=pui)
-    df3 = fld2D(job, t3, pui=pui)
-    fig, axes = plt.subplots(figsize=(10,4), nrows=1, ncols=3)
-    ax1 = axes[0]
-    ax2 = axes[1]
-    ax3 = axes[2]
-    X, Y = np.meshgrid(df1.x, df2.x)
-    c1 = ax1.pcolormesh(X, Y, df1.np, cmap='jet', vmin=vmin, vmax=vmax, rasterized=True)
-    c2 = ax2.pcolormesh(X, Y, df2.np, cmap='jet', vmin=vmin, vmax=vmax, rasterized=True)
-    c3 = ax3.pcolormesh(X, Y, df3.np, cmap='jet', vmin=vmin, vmax=vmax, rasterized=True)
-    ax1.set_ylim(ymin, ymax)
-    ax2.set_ylim(ymin, ymax)
-    ax3.set_ylim(ymin, ymax)
-    ax1.set_ylabel('y')
-    ax1.set_xlabel('x')
-    ax2.set_xlabel('x')
-    ax3.set_xlabel('x')
-    ax1.set_title('$\Omega t='+str(t1*job.nt))
-    ax2.set_title('$\Omega t='+str(t2*job.nt))
-    ax3.set_title('$\Omega t='+str(t3*job.nt))
+
+    df = []
+    c  = []
+    tm = [t1, t2, t3]
+
+    for i in range(3):
+        tmp = fld2D(job, tm[i], pui=pui)
+        df.append(tmp)
+
+    fig, axes = plt.subplots(figsize=(10,6), nrows=1, ncols=3)
+    X, Y = np.meshgrid(df[0].x, df[0].x)
+
+    for i in range(3):
+        tmp = ax[i].pcolormesh(X, Y, df[i].np, cmap='jet', vmin=vmin, vmax=vmax, rasterized=True)
+        c.append(tmp)
+        ax[i].set_ylim(ymin, ymax)
+        ax[i].set_xlabel('x')
+        ax[i].set_title('$\Omega_p t='+str(tm[i]*job.nt))
+    ax[0].set_ylabel('y')
+        
     cax = fig.add_axes([0.91, 0.11, 0.01, 0.77])
     cbar = fig.colorbar(c3, cax = cax, orientation='vertical', format=ptick.FuncFormatter(fmt))
     plt.savefig(jobid+'.tiff')
