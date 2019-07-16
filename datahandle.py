67a6f2f50b35f09f35bcfbeccf9b278c1bd44b7c
tsubok1969 <kent1@mac.com>
2019-07-16 17:02:33 +0900

    corresponding to the change of binary format

--- datahandle.py
+++ datahandle.py
@@ -2,9 +2,10 @@ import numpy as np
 
 #class
 class job1d:
-    def __init__(self,jobid,mpi=False,pui=False,endian=False):
+    def __init__(self,jobid,mpi=False,pui=False,endian=False,old_format=False):
         header = header_endian(endian)
         info = readinfo(jobid, header, "data.inp")
+        self.form = old_format
         self.endian = endian
         self.info = info
         self.id = jobid
@@ -25,9 +26,10 @@ class job1d:
             self.mass = float(info[1])
 
 class job2d:
-    def __init__(self, jobid, mpi=False, pui=False, endian=False):
+    def __init__(self, jobid, mpi=False, pui=False, endian=False, old_format=False):
         header = header_endian(endian)
         info = readinfo(jobid, header, "data.inp")
+        self.form = old_format
         self.endian = endian
         self.info = info
         self.id = jobid
@@ -56,19 +58,19 @@ class fld1D():
         self.x = np.linspace(0., self.job.dx*(self.job.nx-1), self.job.nx)
 
         endian = job.endian
-        index = ("ex","ey","ez","by","bz","np","pl","pp")
+        index = ("by", "bz", "ex","ey","ez","np","pl","pp")
         file = []
         for i in range(8):
             f = fileinfo(job, time, index[i])
             file.append(f)
-        self.by = read1Ddata(file[0], endian, self.job.nx)
-        self.bz = read1Ddata(file[1], endian, self.job.nx)
-        self.ex = read1Ddata(file[2], endian, self.job.nx)
-        self.ey = read1Ddata(file[3], endian, self.job.nx)
-        self.ez = read1Ddata(file[4], endian, self.job.nx)
-        self.np = read1Ddata(file[5], endian, self.job.nx)
-        self.pl = read1Ddata(file[6], endian, self.job.nx)
-        self.pp = read1Ddata(file[7], endian, self.job.nx)
+        self.by = read1Ddata(file[0], endian, self.job.nx, self.job.form)
+        self.bz = read1Ddata(file[1], endian, self.job.nx, self.job.form)
+        self.ex = read1Ddata(file[2], endian, self.job.nx, self.job.form)
+        self.ey = read1Ddata(file[3], endian, self.job.nx, self.job.form)
+        self.ez = read1Ddata(file[4], endian, self.job.nx, self.job.form)
+        self.np = read1Ddata(file[5], endian, self.job.nx, self.job.form)
+        self.pl = read1Ddata(file[6], endian, self.job.nx, self.job.form)
+        self.pp = read1Ddata(file[7], endian, self.job.nx, self.job.form)
 
         bx = np.cos(np.deg2rad(job.th))
         self.bf = np.sqrt(bx**2 + self.by**2 + self.bz**2)
@@ -81,9 +83,9 @@ class fld1D():
             for i in range(3):
                 f = fileinfo(job, time, index[i])
                 file.append(f)
-            self.npui = read1Ddata(file[0], endian, self.job.nx)
-            self.ppui = read1Ddata(file[1], endian, self.job.nx)
-            self.prui = read1Ddata(file[2], endian, self.job.nx)
+            self.npui = read1Ddata(file[0], endian, self.job.nx, self.job.form)
+            self.ppui = read1Ddata(file[1], endian, self.job.nx, self.job.form)
+            self.prui = read1Ddata(file[2], endian, self.job.nx, self.job.form)
             
 class fld2D():
     def __init__(self,job,time,pui=False):
@@ -95,18 +97,18 @@ class fld2D():
         for i in range(12):
             f = fileinfo(job, time, index[i])
             file.append(f)
-        self.bx = read2Ddata(file[0], endian, self.job.nx, self.job.ny)
-        self.by = read2Ddata(file[1], endian, self.job.nx, self.job.ny)
-        self.bz = read2Ddata(file[2], endian, self.job.nx, self.job.ny)
-        self.ex = read2Ddata(file[3], endian, self.job.nx, self.job.ny)
-        self.ey = read2Ddata(file[4], endian, self.job.nx, self.job.ny)
-        self.ez = read2Ddata(file[5], endian, self.job.nx, self.job.ny)
-        self.ux = read2Ddata(file[6], endian, self.job.nx, self.job.ny)
-        self.uy = read2Ddata(file[7], endian, self.job.nx, self.job.ny)
-        self.uz = read2Ddata(file[8], endian, self.job.nx, self.job.ny)
-        self.np = read2Ddata(file[9], endian, self.job.nx, self.job.ny)
-        self.pp = read2Ddata(file[10], endian, self.job.nx, self.job.ny)
-        self.pl = read2Ddata(file[11], endian, self.job.nx, self.job.ny)
+        self.bx = read2Ddata(file[0], endian, self.job.nx, self.job.ny, self.job.form)
+        self.by = read2Ddata(file[1], endian, self.job.nx, self.job.ny, self.job.form)
+        self.bz = read2Ddata(file[2], endian, self.job.nx, self.job.ny, self.job.form)
+        self.ex = read2Ddata(file[3], endian, self.job.nx, self.job.ny, self.job.form)
+        self.ey = read2Ddata(file[4], endian, self.job.nx, self.job.ny, self.job.form)
+        self.ez = read2Ddata(file[5], endian, self.job.nx, self.job.ny, self.job.form)
+        self.ux = read2Ddata(file[6], endian, self.job.nx, self.job.ny, self.job.form)
+        self.uy = read2Ddata(file[7], endian, self.job.nx, self.job.ny, self.job.form)
+        self.uz = read2Ddata(file[8], endian, self.job.nx, self.job.ny, self.job.form)
+        self.np = read2Ddata(file[9], endian, self.job.nx, self.job.ny, self.job.form)
+        self.pp = read2Ddata(file[10], endian, self.job.nx, self.job.ny, self.job.form)
+        self.pl = read2Ddata(file[11], endian, self.job.nx, self.job.ny, self.job.form)
 
         self.bf = np.sqrt(self.bx**2+self.by**2+self.bz**2)
 
@@ -119,9 +121,9 @@ class fld2D():
             for i in range(3):
                 f = fileinfo(job,time,index[i])
                 file.append(f)
-            self.npui = read2Ddata(file[0], endian, self.job.nx, self.job.ny)
-            self.ppui = read2Ddata(file[1], endian, self.job.nx, self.job.ny)
-            self.prui = read2Ddata(file[2], endian, self.job.nx, self.job.ny)
+            self.npui = read2Ddata(file[0], endian, self.job.nx, self.job.ny, self.job.form)
+            self.ppui = read2Ddata(file[1], endian, self.job.nx, self.job.ny, self.job.form)
+            self.prui = read2Ddata(file[2], endian, self.job.nx, self.job.ny, self.job.form)
 
 class ptl1D():
     def __init__(self, job, time, mpi=False, proc=None, pui=False):
@@ -129,11 +131,9 @@ class ptl1D():
         self.time = time * job.tp
         endian = job.endian
         if pui:
-#            index = ('vxui','vyui','vzui','xpui','ypui')
             index = ('vxui','vyui','vzui','xpui')
             ntmp = job.npui
         else:
-#            index = ('vx','vy','vz','xp','yp')
             index = ('vx','vy','vz','xp')
             ntmp = job.np
         file = []
@@ -145,11 +145,10 @@ class ptl1D():
             self.np = iem - ism + 1
         else:
             self.np = ntmp
-        self.vx = read1Ddata(file[0], endian, self.np)
-        self.vy = read1Ddata(file[1], endian, self.np)
-        self.vz = read1Ddata(file[2], endian, self.np)
-        self.xp = read1Ddata(file[3], endian, self.np)
-#        self.yp = read1Ddata(file[4], endian, self.np)
+        self.vx = read1Ddata(file[0], endian, self.np, self.job.form)
+        self.vy = read1Ddata(file[1], endian, self.np, self.job.form)
+        self.vz = read1Ddata(file[2], endian, self.np, self.job.form)
+        self.xp = read1Ddata(file[3], endian, self.np, self.job.form)
         self.ke = 0.5*(self.vx**2+self.vy**2+self.vz**2)
         if pui:
             self.ke = job.mass * self.ke
@@ -162,7 +161,7 @@ class ptl2D(ptl1D):
         else:
             index= 'yp'
         f = fileinfo(job, time, index, mpi, proc)
-        self.yp = read1Ddata(f, job.endian, self.np)
+        self.yp = read1Ddata(f, job.endian, self.np, self.job.form)
             
 # function
 def header_endian(endian):
@@ -191,28 +190,38 @@ def fileinfo(job, time, index, mpiproc=False, proc=None):
         datloc = header + jobid + "/" + index + itime + '.dat'
     return datloc
 
-def read1Ddata(file, endian, nx):
+def read1Ddata(file, endian, nx, old_format):
     f = open(file, "rb")
-    dty = set_datatype(endian, nx)
-    dtmp = np.fromfile(f, dtype=dty, count=1)
+    dty = set_datatype(endian, nx, old_format)
+    dtmp = np.fromfile(f, dtype=dty, count=-1)
     data = dtmp[0]['tmp']
     f.close()
     return data
 
-def read2Ddata(file, endian, nx, ny):
+def read2Ddata(file, endian, nx, ny, old_format=False):
     size = nx*ny
-    data = read1Ddata(file, endian, size).reshape(ny,nx)
+    data = read1Ddata(file, endian, size, old_format).reshape(ny,nx)
     return data
 
-def set_datatype(endian, size):
+def set_datatype(endian, size, old_format):
     if endian:
-        head = ('head','>i')
-        tail = ('tail','>i')
-        dty = np.dtype([head, ('tmp','>'+str(size)+'f'), tail])
+#        head = ('head','>i')
+#        tail = ('tail','>i')
+        head = ('head','>2i')
+        tail = ('tail','>2i')
+        data = ('tmp','>'+str(size)+'f')
+#        dty = np.dtype([head, ('tmp','>'+str(size)+'f'), tail])
     else:
         head = ('head','<i')
         tail = ('tail','<i')
-        dty = np.dtype([head, ('tmp','<'+str(size)+'f'), tail])
+        data = ('tmp','<'+str(size)+'f')
+#        dty = np.dtype([head, ('tmp','<'+str(size)+'f'), tail])
+
+    if old_format:
+        dty = np.dtype([head, data, tail])
+    else:
+        dty = np.dtype([data])
+
     return dty
 
 def mpiset(is0, ie0, iproc, irank):
